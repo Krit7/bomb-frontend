731baeccca419ec8a273a68e8a5a2b4be9025667
Krit Verma <kritverma@Krits-MacBook-Air.local>
2022-03-25 00:59:13 +0530

    feat: dashboard and finance summary structure

--- src/views/Dashboard/Components/FinanceSummary.tsx
+++ src/views/Dashboard/Components/FinanceSummary.tsx
@@ -1,12 +1,56 @@
-import React, {useCallback, useMemo} from 'react';
-
-
-import HomeImage from '../../assets/img/background.jpg';
+import React, { useCallback, useMemo } from 'react';
+import useBombStats from '../../../hooks/useBombStats';
+import useLpStats from '../../../hooks/useLpStats';
+import useLpStatsBTC from '../../../hooks/useLpStatsBTC';
+import useBondStats from '../../../hooks/useBondStats';
+import usebShareStats from '../../../hooks/usebShareStats';
+import useBombFinance from '../../../hooks/useBombFinance';
 import { Grid, Box } from '@material-ui/core';
+import { roundAndFormatNumber } from '../../../0x';
+const FinanceSummary: React.FC = () => {
+  const bombFtmLpStats = useLpStatsBTC('BOMB-BTCB-LP');
+  const bShareFtmLpStats = useLpStats('BSHARE-BNB-LP');
+  const bombStats = useBombStats();
+  const bShareStats = usebShareStats();
+  const tBondStats = useBondStats();
+  const bombFinance = useBombFinance();
+  const bombLPStats = useMemo(() => (bombFtmLpStats ? bombFtmLpStats : null), [bombFtmLpStats]);
+  const bshareLPStats = useMemo(() => (bShareFtmLpStats ? bShareFtmLpStats : null), [bShareFtmLpStats]);
+  const bombPriceInDollars = useMemo(
+    () => (bombStats ? Number(bombStats.priceInDollars).toFixed(2) : null),
+    [bombStats],
+  );
+  const bombPriceInBNB = useMemo(() => (bombStats ? Number(bombStats.tokenInFtm).toFixed(4) : null), [bombStats]);
+  const bombCirculatingSupply = useMemo(() => (bombStats ? String(bombStats.circulatingSupply) : null), [bombStats]);
+  const bombTotalSupply = useMemo(() => (bombStats ? String(bombStats.totalSupply) : null), [bombStats]);
 
-const FinanceSummary:React.FC = () => {
-    return (
-        <div>
+  const bSharePriceInDollars = useMemo(
+    () => (bShareStats ? Number(bShareStats.priceInDollars).toFixed(2) : null),
+    [bShareStats],
+  );
+  const bSharePriceInBNB = useMemo(
+    () => (bShareStats ? Number(bShareStats.tokenInFtm).toFixed(4) : null),
+    [bShareStats],
+  );
+  const bShareCirculatingSupply = useMemo(
+    () => (bShareStats ? String(bShareStats.circulatingSupply) : null),
+    [bShareStats],
+  );
+  const bShareTotalSupply = useMemo(() => (bShareStats ? String(bShareStats.totalSupply) : null), [bShareStats]);
+
+  const tBondPriceInDollars = useMemo(
+    () => (tBondStats ? Number(tBondStats.priceInDollars).toFixed(2) : null),
+    [tBondStats],
+  );
+  const tBondPriceInBNB = useMemo(() => (tBondStats ? Number(tBondStats.tokenInFtm).toFixed(4) : null), [tBondStats]);
+  const tBondCirculatingSupply = useMemo(
+    () => (tBondStats ? String(tBondStats.circulatingSupply) : null),
+    [tBondStats],
+  );
+  const tBondTotalSupply = useMemo(() => (tBondStats ? String(tBondStats.totalSupply) : null), [tBondStats]);
+
+  return (
+    <div style={{ color: 'white' }}>
       <h2>FINANCE SUMMARY</h2>
       <hr></hr>
       <Grid container spacing={3}>
@@ -29,13 +73,14 @@ const FinanceSummary:React.FC = () => {
               COL-11
             </Grid>
             <Grid item xs={2}>
-              COL-12
+              {roundAndFormatNumber(parseInt(bombCirculatingSupply), 2)}
             </Grid>
             <Grid item xs={2}>
-              COL-13
+              {roundAndFormatNumber(parseInt(bombTotalSupply), 2)}
             </Grid>
             <Grid item xs={3}>
-              COL-14
+              <span style={{display: 'block', textAlign:'center'}}>${bombPriceInDollars ? roundAndFormatNumber(parseInt(bombPriceInDollars),5) : '-.--'}</span>
+              <span>{bombPriceInBNB ? bombPriceInBNB : '-.----'} BTC</span>
             </Grid>
             <Grid item xs={2}>
               COL-15
@@ -84,7 +129,7 @@ const FinanceSummary:React.FC = () => {
         </Grid>
       </Grid>
     </div>
-    )
+  )
 }
 
 export default FinanceSummary;
--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -4,28 +4,10 @@ import {createGlobalStyle} from 'styled-components';
 import {Route, Switch, useRouteMatch} from 'react-router-dom';
 import {useWallet} from 'use-wallet';
 import UnlockWallet from '../../components/UnlockWallet';
-import PageHeader from '../../components/PageHeader';
-import styled from 'styled-components';
-import Spacer from '../../components/Spacer';
-import useBondStats from '../../hooks/useBondStats';
-import useBombFinance from '../../hooks/useBombFinance';
-import useCashPriceInLastTWAP from '../../hooks/useCashPriceInLastTWAP';
-import {useTransactionAdder} from '../../state/transactions/hooks';
-import useTokenBalance from '../../hooks/useTokenBalance';
-import useBondsPurchasable from '../../hooks/useBondsPurchasable';
-import {getDisplayBalance} from '../../utils/formatBalance';
-import { BOND_REDEEM_PRICE, BOND_REDEEM_PRICE_BN } from '../../bomb-finance/constants';
-import { Alert } from '@material-ui/lab';
-import useBondStats from '../../../hooks/useBondStats';
-import usebShareStats from '../../../hooks/usebShareStats';
-import useBombStats from '../../../hooks/useBombStats';
-import useLpStats from '../../../hooks/useLpStats';
-import useLpStatsBTC from '../../hooks/useLpStatsBTC';
-
-import HomeImage from '../../assets/img/background.jpg';
 import { Grid, Box } from '@material-ui/core';
 import FinanceSummary from './Components/FinanceSummary'
-
+import HomeImage from '../../assets/img/background.jpg';
+import styled from 'styled-components';
 const BackgroundImage = createGlobalStyle`
   body {
     background: url(${HomeImage}) repeat !important;
@@ -35,48 +17,7 @@ const BackgroundImage = createGlobalStyle`
 `;
 
 const Dashboard:React.FC = () => {
-  const classes = useStyles();
-  const TVL = useTotalValueLocked();
-  const bombFtmLpStats = useLpStatsBTC('BOMB-BTCB-LP');
-  const bShareFtmLpStats = useLpStats('BSHARE-BNB-LP');
-  const bombStats = useBombStats();
-  const bShareStats = usebShareStats();
-  const tBondStats = useBondStats();
-  const bombFinance = useBombFinance();
-  const bombLPStats = useMemo(() => (bombFtmLpStats ? bombFtmLpStats : null), [bombFtmLpStats]);
-  const bshareLPStats = useMemo(() => (bShareFtmLpStats ? bShareFtmLpStats : null), [bShareFtmLpStats]);
-  const bombPriceInDollars = useMemo(
-    () => (bombStats ? Number(bombStats.priceInDollars).toFixed(2) : null),
-    [bombStats],
-  );
-  const bombPriceInBNB = useMemo(() => (bombStats ? Number(bombStats.tokenInFtm).toFixed(4) : null), [bombStats]);
-  const bombCirculatingSupply = useMemo(() => (bombStats ? String(bombStats.circulatingSupply) : null), [bombStats]);
-  const bombTotalSupply = useMemo(() => (bombStats ? String(bombStats.totalSupply) : null), [bombStats]);
-
-  const bSharePriceInDollars = useMemo(
-    () => (bShareStats ? Number(bShareStats.priceInDollars).toFixed(2) : null),
-    [bShareStats],
-  );
-  const bSharePriceInBNB = useMemo(
-    () => (bShareStats ? Number(bShareStats.tokenInFtm).toFixed(4) : null),
-    [bShareStats],
-  );
-  const bShareCirculatingSupply = useMemo(
-    () => (bShareStats ? String(bShareStats.circulatingSupply) : null),
-    [bShareStats],
-  );
-  const bShareTotalSupply = useMemo(() => (bShareStats ? String(bShareStats.totalSupply) : null), [bShareStats]);
-
-  const tBondPriceInDollars = useMemo(
-    () => (tBondStats ? Number(tBondStats.priceInDollars).toFixed(2) : null),
-    [tBondStats],
-  );
-  const tBondPriceInBNB = useMemo(() => (tBondStats ? Number(tBondStats.tokenInFtm).toFixed(4) : null), [tBondStats]);
-  const tBondCirculatingSupply = useMemo(
-    () => (tBondStats ? String(tBondStats.circulatingSupply) : null),
-    [tBondStats],
-  );
-  const tBondTotalSupply = useMemo(() => (tBondStats ? String(tBondStats.totalSupply) : null), [tBondStats]);
+    const {account} = useWallet();
     return (
         <Switch>
             <Page>
